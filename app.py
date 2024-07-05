501e0353d91b4182e31b022cc6eed90be810a161
brez7 <rob@barbank.com>
2024-07-05 13:20:29 -0700

    d
    d

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", is_index=True)
 
- 
+
+@app.route("/removed")
+def removed():
+    return render_template("index-removed.html", is_index=False)
+
+
 if __name__ == "__main__":
     server_port = int(os.environ.get("PORT", "8080"))
     app.run(debug=True, port=server_port, host="0.0.0.0")
